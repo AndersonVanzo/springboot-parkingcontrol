782febcd11b0415bba6db9ef4a8738385b36b45a
Anderson R. Vanzo <anderson.vanzo01@gmail.com>
2023-04-08 16:47:50 -0300

    feat: roles and access control

--- src/main/java/com/knownt/parkingcontrol/configs/security/UserDetailsServiceImpl.java
+++ src/main/java/com/knownt/parkingcontrol/configs/security/UserDetailsServiceImpl.java
@@ -1,12 +1,16 @@
 package com.knownt.parkingcontrol.configs.security;
 
+import com.knownt.parkingcontrol.models.UserModel;
 import com.knownt.parkingcontrol.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional
 public class UserDetailsServiceImpl implements UserDetailsService {
 	final UserRepository userRepository;
 
@@ -16,7 +20,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		return userRepository.findByUsername(username)
-				.orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+		UserModel userModel = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+		return new User(userModel.getUsername(), userModel.getPassword(), true, true, true, true, userModel.getAuthorities());
 	}
 }
---
+++ src/main/java/com/knownt/parkingcontrol/models/RoleModel.java
@@ -0,0 +1,46 @@
+package com.knownt.parkingcontrol.models;
+
+import com.knownt.parkingcontrol.enums.RoleName;
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_ROLE")
+
+public class RoleModel implements GrantedAuthority, Serializable {
+	@Serial
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, unique = true)
+	private RoleName roleName;
+
+	@Override
+	public String getAuthority() {
+		return this.roleName.toString();
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public RoleName getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(RoleName roleName) {
+		this.roleName = roleName;
+	}
+}
